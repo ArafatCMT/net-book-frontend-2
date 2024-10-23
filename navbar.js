a0174e446eda16d35c0cf3c800485316834db015
ArafatCMT <arafatemon6969@gmail.com>
2024-10-23 13:13:56 +0600

    update done

--- navbar.js
+++ navbar.js
@@ -13,19 +13,19 @@ fetch("navbar.html")
 
     if (token){
         const parent = document.getElementById("net-book")
-        parent.innerHTML = `<a class="navbar-brand" id="navbar-brand" href="./home.html" style="color:#0F6FEC;">Net Book</a>`
+        parent.innerHTML = `<a class="navbar-brand" id="navbar-brand" href="./home.html" style="color:#06537A;"><b>Net Book</b></a>`
         navLeftSideElement.innerHTML += `
                             <li class="nav-item mx-4">
-                                <a class="nav-link active" aria-current="page" href="./home.html"><i class="fa-solid fa-house fs-4" style="color:#0F6FEC;"></i></a>
+                                <a class="nav-link active" aria-current="page" href="./home.html"><i class="fa-solid fa-house fs-4" style="color:#06537A;"></i></a>
                             </li>
                             <li class="nav-item mx-4">
-                                <a class="nav-link addPost"  data-bs-toggle="modal" data-bs-target="#createPostModal"><i class="fa-solid fa-square-plus fs-4" style="color:#0F6FEC;"></i></a>
+                                <a class="nav-link addPost home"  data-bs-toggle="modal" data-bs-target="#createPostModal"><i class="fa-solid fa-square-plus fs-4" style="color:#06537A;"></i></a>
                             </li>
                             <li class="nav-item mx-4">
-                                <a class="nav-link" href="./friends.html"><i class="fa-solid fa-user-group fs-4" style="color:#0F6FEC;"></i></a>
+                                <a class="nav-link" href="./friends.html"><i class="fa-solid fa-user-group fs-4" style="color:#06537A;"></i></a>
                             </li>
                             <li class="nav-item mx-4">
-                                <a class="nav-link" href="./request.html"><i class="fa-solid fa-user-plus fs-4" style="color:#0F6FEC;"></i></a>
+                                <a class="nav-link" href="./request.html"><i class="fa-solid fa-user-plus fs-4" style="color:#06537A;"></i></a>
                             </li>
                             
                             `;
